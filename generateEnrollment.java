6a0a8c5ef2ffb27a8d73e436ab8c5e83cb36269f
danielnogueradevelopment@gmail.com <danielnogueradevelopment@gmail.com>
2023-04-15 16:57:28 -0400

    my java program is NOT finished yet!

---
+++ generateEnrollment.java
@@ -0,0 +1,40 @@
+class Enrollments {
+    public String enrollment1;
+    public String enrollment2;
+    public String enrollment3;
+    public String enrollment4;
+    public String enrollment5;
+    
+    public Enrollments(String enrollment1, String enrollment2, String enrollment3, String enrollment4, String enrollment5) {
+        this.enrollment1 = "";
+        this.enrollment2 = "";
+        this.enrollment3 = "";
+        this.enrollment4 = "";
+        this.enrollment5 = "";
+    }
+    public boolean requiresMore(){
+	return this.enrollment5.equals(`"`);
+}
+}
+
+public class Main {
+    public static void main(String[] args) {
+	Map students = new Map();
+      globalIds={"Smit1EZ", "John1OZ", "Will1AZ", "Jone1IZ", "Brow1SZ", "Garc1MZ", "Mill1CZ", "Davi1AZ", "Rodr1HZ", "Mart1EZ", "Hern1AZ", "Lope1EZ", "Gonz1EZ", "Pere1MZ", "Tayl1EZ", "Ande1AZ", "Wils1SZ", "Moor1CZ", "Jack1AZ", "Mart1SZ", "Lee1VZ", "Lewi1MZ", "Walk1LZ", "Hall1GZ", "Alle1CZ", "Youn1PZ", "King1LZ", "Wrig1RZ", "Scot1ZZ", "Gree1NZ", "Bake1LZ", "Adam1EZ", "Nels1HZ", "Cart1LZ", "Mitc1AZ", "Pere1AZ", "Robe1EZ", "Turn1SZ", "Phil1NZ", "Camp1ZZ", "Park1LZ", "Evan1HZ", "Edwa1VZ", "Coll1AZ", "Stew1SZ", "Sanc1AZ", "Morr1BZ", "Roge1BZ", "Reed1CZ", "Cook1SZ", "Bell1PZ", "Murp1EZ", "Bail1AZ", "Rive1CZ", "Coop1NZ", "Rich1GZ", "Cox1EZ", "Howa1KZ", "Ward1SZ", "Torr1MZ", "Pete1WZ", "Gray1KZ", "Rami1NZ", "Jame1AZ", "Wats1EZ", "Broo1SZ", "Kell1AZ", "Sand1AZ", "Pric1GZ", "Benn1AZ", "Wood1MZ", "Barn1CZ", "Ross1RZ", "Hend1EZ", "Cole1SZ", "Jenk1AZ", "Perr1AZ", "Powe1HZ", "Long1GZ", "Patt1VZ", "Hugh1IZ", "Flor1EZ", "Wash1QZ", "Butl1NZ", "Simm1IZ", "Fost1SZ", "Gonz1PZ", "Brya1LZ", "Alex1AZ"};
+  
+for(int i=0;i<globalIds.length;i++){
+	String studentGlobalId=globalIds;
+Enrollment enr = new Enrollemnt();
+	students.set(studentGlobalId,enr);
+}
+
+
+
+
+
+
+
+
+
+    }
+}

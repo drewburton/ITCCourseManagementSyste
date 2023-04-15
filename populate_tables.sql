4da803900570aba793b31674e32abcc3fb6cf0ff
danielnogueradevelopment@gmail.com <danielnogueradevelopment@gmail.com>
2023-04-15 14:50:58 -0400

    moved over all replit data

---
+++ populate_tables.sql
@@ -0,0 +1,39 @@
+INSERT INTO Professors (GlobalId, FirstName, LastName, Salary)
+VALUES
+  ('P0000001', 'John', 'Doe', 100000.00),
+  ('P0000002', 'Jane', 'Doe', 95000.00),
+  ('P0000003', 'Bob', 'Smith', 85000.00),
+  ('P0000004', 'Mary', 'Johnson', 90000.00),
+  ('P0000005', 'Tom', 'Brown', 80000.00),
+  ('P0000006', 'Lisa', 'Garcia', 110000.00),
+  ('P0000007', 'David', 'Lee', 120000.00),
+  ('P0000008', 'Karen', 'Davis', 95000.00),
+  ('P0000009', 'Mike', 'Jones', 105000.00),
+  ('P0000010', 'Sarah', 'Wilson', 90000.00);
+
+
+
+
+
+
+INSERT INTO Courses (CourseId, Department, Title, IsWritingIntensive) VALUES
+  (101, 'MAT', 'Calculus I', false),
+  (102, 'MAT', 'Calculus II', false),
+  (201, 'PHY', 'Physics I', false),
+  (202, 'PHY', 'Physics II', false),
+  (301, 'CHE', 'Organic Chemistry I', true),
+  (302, 'CHE', 'Organic Chemistry II', true),
+  (202, 'ENG', 'Introduction to Creative Writing', true),
+  (402, 'ENG', 'Shakespeare and His Contemporaries', false),
+  (230, 'HIS', 'American History: Revolution to Civil War', true),
+  (360, 'HIS', 'American History: Reconstruction to Present', true),
+  (161, 'BIO', 'Introductory Biology I', false),
+  (162, 'BIO', 'Introductory Biology II', false),
+  (141, 'PSY', 'Introduction to Psychology', true),
+  (460, 'PSY', 'Abnormal Psychology', true),
+  (120, 'ECO', 'Principles of Microeconomics', false),
+  (121, 'ECO', 'Principles of Macroeconomics', false),
+  (127, 'ART', 'Drawing I', true),
+  (232, 'ART', 'Painting II', true),
+  (101, 'MUS', 'Music Theory I', true),
+  (152, 'MUS', 'Music History: Baroque to Classical', false);

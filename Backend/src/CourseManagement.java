8a86f01c6a0b65329e5d9017b1219218f6f3d3fd
drewburton <drew.burton.gc@gmail.com>
2023-04-19 13:45:56 -0400

    made sample frontend with classes

--- Backend/src/CourseManagement.java
+++ Backend/src/CourseManagement.java
@@ -5,31 +5,70 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
  * The measurement units for graphics in Java are all in pixels
  */
 public class CourseManagement extends Application {
+	private ObservableList<String> availableCourses = FXCollections.observableArrayList(
+            "Math 101", "History 201", "Science 301", "English 401", "Computer Science 501");
+
+    private ListView<String> courseListView = new ListView<>();
+    private ListView<String> registeredCoursesListView = new ListView<>();
+    
     @Override // Override the start method in the Application class
     public void start(Stage primaryStage) {
     	Connection conn;
 		try {
 			conn = connectToDatabase();
-			Scene scene = createScene(getTestCol(conn));
-			primaryStage.setTitle("Course Management System"); // Set the stage title
-			primaryStage.setScene(scene); // Place the scene in the stage
-			primaryStage.show(); // Display the stage
 		} catch (Exception e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			return;
 		}
 		
-		        
+	       // Set up UI components
+        Label availableCoursesLabel = new Label("Available Courses:");
+        courseListView.setItems(availableCourses);
+        courseListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        Label registeredCoursesLabel = new Label("Registered Courses:");
+        registeredCoursesListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        Button registerButton = new Button("Register");
+        registerButton.setOnAction(event -> registerCourses());
+
+        // Set up UI layout
+        VBox leftBox = new VBox(10, availableCoursesLabel, courseListView);
+        VBox rightBox = new VBox(10, registeredCoursesLabel, registeredCoursesListView);
+        VBox.setVgrow(courseListView, Priority.ALWAYS);
+        VBox.setVgrow(registeredCoursesListView, Priority.ALWAYS);
+
+        HBox centerBox = new HBox(10, leftBox, rightBox);
+        centerBox.setPadding(new Insets(10));
+
+        BorderPane root = new BorderPane(centerBox, null, null, registerButton, null);
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("Course Registration App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
         try {
 			conn.close();
 		} catch (SQLException e) {
@@ -62,22 +101,10 @@ public class CourseManagement extends Application {
         return rs.getString("testCol");	
     }
     
-    public static Scene createScene(String val) {
-        // Create a TextField
-        TextField textField = new TextField();
-
-        // Set the text to "hello"
-        textField.setText(val);
-
-        // Make the TextField unmodifiable
-        textField.setEditable(false);        
-                
-        // Create a pane to hold the circle
-        StackPane pane = new StackPane();
-        pane.getChildren().add(textField);
-
-        // Create a scene and place it in the stage
-        Scene scene = new Scene(pane, 200, 200);
-        return scene;
+    private void registerCourses() {
+        ObservableList<String> selectedCourses = courseListView.getSelectionModel().getSelectedItems();
+        registeredCoursesListView.getItems().addAll(selectedCourses);
+        courseListView.getItems().removeAll(selectedCourses);
+        courseListView.getSelectionModel().clearSelection();
     }
 }

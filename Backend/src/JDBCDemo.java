98cb9eac8d8e00d4a8c96b55a196db3afedc17bd
drewburton <drew.burton.gc@gmail.com>
2023-04-19 13:40:37 -0400

    made demo frontend work pulling from test table in database

--- Backend/src/JDBCDemo.java
+++ Backend/src/JDBCDemo.java
@@ -5,16 +5,16 @@ import java.sql.Statement;
 
 public class JDBCDemo {
     public static void main(String args[]) throws Exception {
-        String query = "select FirstName || ' ' || LastName as NAME, SALARY from instructors";
         Class.forName("oracle.jdbc.driver.OracleDriver");
         String url = "jdbc:oracle:thin:@localhost:1521/pdborcl";
         Connection conn = DriverManager.getConnection(url, "drew", "setrabp");
         Statement stmt = conn.createStatement();
+        
+        String query = "select testcol from test";
         ResultSet rs = stmt.executeQuery(query);
         while (rs.next()) {
-            String name = rs.getString("NAME");
-            int salary = rs.getInt("SALARY");
-            System.out.println(name + " earns " + salary);
+            String test = rs.getString("TESTCOL");
+            System.out.println(test);
         }
         conn.close();
     }

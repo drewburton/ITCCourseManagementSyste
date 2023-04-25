bd49070695139773d4e753444c699efd70325a2f
DanielNogueraDevelopment <50124094+DanielNogueraDevelopment@users.noreply.github.com>
2023-04-24 23:05:38 -0400

    improved readability

--- challengequery.sql
+++ challengequery.sql
@@ -1,8 +1,9 @@
+--challenge query 1
 SELECT Title, (Department || courseId) as course_Identifier 
 FROM Courses 
 WHERE Courses.CourseId in 
 (SELECT Courses.CourseId FROM Courses, Sessions WHERE Courses.CourseId=Sessions.CourseId and Sessions.SessionId in 
 (SELECT SessionId FROM Enrollment WHERE StudentGlobalId = 'Davi1AZ'))
-
+--chellenge query 2
 SELECT (FirstName || ' ' || LastName) as Full_Name FROM Students WHERE Students.globalId in ( 
 SELECT StudentGlobalId FROM Enrollment WHERE SessionId='5');
